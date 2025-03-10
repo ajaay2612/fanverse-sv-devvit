9b6dc306bafc2e4f833b8ae95d0f525c22c97302
ajaay <ajaay2612@gmail.com>
2025-03-10 20:12:10 +0530

    initialize fanverse project structure with configuration files and basic components

---
+++ fanverse-sv/src/lib/components/FanButton.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+<div class="relative w-[7.5em]">
+    <div class="w-[3.3em] absolute top-[-0.32em] left-1/2 -translate-x-1/2">
+        <svg  viewBox="0 0 79 4"  xmlns="http://www.w3.org/2000/svg">
+            <path class="fill-(--color-sec)" d="M0 4L5.17467 0H73.8253L79 4H0Z" />
+        </svg>                
+    </div>
+    <button class="orange-button">
+        <slot/>
+    </button>
+    <div class="w-[1.8em] absolute bottom-[-0.2em] left-1/2 -translate-x-1/2">
+        <svg viewBox="0 0 44 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path class="fill-(--color-sec)" d="M0 0L2.8821 2H41.1179L44 0H0Z"/>
+        </svg>                                         
+    </div>
+</div>
---
+++ fanverse-sv/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import FanButton from '../lib/components/FanButton.svelte';
+
+
+</script>
+
+
+
+<div class="homeScreenGradient h-screen w-full flex justify-center flex-col gap-[2.1em] items-center">
+    <div class="pt-[1.8em] text-[1.1em] rotate-[-5deg] origin-bottom-left ">
+        <h1 class="leading-[1em] font-inter-italic font-bold text-pri text-[2.8em]">FAN<span class="text-stroke text-black">VERSE</span></h1>
+        <p class="text-[0.75em] text-right leading-[1em] font-inter-italic font-bold text-pri">WHERE FANS <span class="text-sec">UNITE</span></p>
+    </div>
+
+
+    <div class="space-y-[1.3em]">
+        <FanButton>create</FanButton>
+        <FanButton>KNOW MORE</FanButton>
+    </div>
+
+</div>
